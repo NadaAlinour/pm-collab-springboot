a427659345c7ce5810fef2d9742a0c4b83e3c336
NadaAlinour <lime.x.na@gmail.com>
2026-08-02 01:35:51 +0300

    Remove jpa specification extension and import

--- backend/src/main/java/com/example/project_management_system/repository/TicketRepository.java
+++ backend/src/main/java/com/example/project_management_system/repository/TicketRepository.java
@@ -3,13 +3,12 @@ package com.example.project_management_system.repository;
 import com.example.project_management_system.entity.Priority;
 import com.example.project_management_system.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long>, JpaSpecificationExecutor<Ticket> {
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
     public List<Ticket> findAllByProjectId(Long id);
 
     @Query("select t from Ticket t where (t.title like concat(:title, '%')) and (:status is null or t.status.id = :status) and (:priority is null or t.priority = :priority) and (:category is null or t.category.id = :category) ")
